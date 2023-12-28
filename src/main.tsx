2068b8e54b64ace592f2586bef1813c106d16b70
Mrigank Doshy <mrigankdoshy@gmail.com>
2023-12-27 23:04:46 -0500

    Switch to ES Lint + Prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.tsx";
-import "./index.css";
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root"),
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
